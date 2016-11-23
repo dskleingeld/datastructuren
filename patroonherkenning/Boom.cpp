a0eb64a16a09248088cc82ad3419f70b155a8413
dskleingeld <dskleingeld@gmail.com>
2016-11-23 15:46:32 +0100

    Boom done in principle

--- patroonherkenning/Boom.cpp
+++ patroonherkenning/Boom.cpp
@@ -2,41 +2,51 @@
 
 Boom::Boom(std::string expression) {
   //call the recursive Tree_building function with as its entrance point the root of the tree
-  root = new Leaf;
+  root = NULL;
   Tree_building(expression, root);
 }
 
 void Boom::Tree_building(std::string str, Leaf* ingang) {
+  //ingang is a new empty pointer with nothing attached
   std::string str_left;
-  std::string str_right = NULL;
-  
-  if(enclosed(str)){
-    str_left = str.substr(1, str.size() -1);
+  std::string str_right;
+  Leaf* goLeft = NULL;
+  Leaf* goRight = NULL;
+
+  if (enclosed(str)) {
+    str_left = str.substr(1, str.size() - 1);
+    goLeft = ingang;
   }
-  else if(repEnclosed(str)){
-    str_left = str.substr(1, str.size() -2);
-    build_rep_tree(ingang);
+  else if (repEnclosed(str)) {
+    str_left = str.substr(1, str.size() - 2);
+    build_rep_tree(ingang, goLeft);
+    goLeft = ingang->left;
   }
-  else if(split_concat(str, str_left, str_right)){
-    build_concat_tree(ingang);
+  else if (split_concat(str, str_left, str_right)) {
+    build_concat_tree(ingang, goLeft, goRight);
+    goLeft = ingang->left;
+    goRight = ingang->right;
   }
-  else if(split_or(str, str_left, str_left)){
-    build_or_tree(ingang);
+  else if (split_or(str, str_left, str_left)) {
+    build_or_tree(ingang, goLeft, goRight);
+    goLeft = ingang->left;
+    goRight = ingang->right;
   }
-  else if(OneLetter(str)){
+  //both oneLetter functions handle all left string work.
+  //further recursion stops here
+  else if (oneLetter(str)) {
     build_letter_tree(str, ingang);
   }
-  else if(RepOneLetter(str)){
-    build_rep_tree(ingang);
-    ingang = ingang->left;
+  else if (repOneLetter(str)) {
+    build_rep_tree(ingang, ingang); //ingang changed here
     build_letter_tree(str, ingang);
   }
-  else{
-    std::stderr<<"Unrecognised structure"<<str;
+  else {
+    std::stderr << "Unrecognised structure" << str;
   }
-  
-  Tree_building(str_left, ingang->left);
-  if(str_right != NULL) Tree_building(str_right, ingang->right);
+
+  if (goLeft != NULL) Tree_building(str_left, goLeft);
+  if (goRight != NULL) Tree_building(str_right, goRight);
 }
 
 bool Boom::enclosed(std::string str) {
@@ -46,20 +56,73 @@ bool Boom::enclosed(std::string str) {
 
 bool Boom::repEnclosed(std::string str) {
   std::string str_ending = str.substr(str.size() -2) //get last 2 chars of string
-  if((str[0]=='(') && (str_ending ==")*") && ) return true;
+  if((str[0]=='(') && (str_ending ==")*")) return true;
   else return false;
 }
 
-bool Boom::split_concat(std::string str, std::string &str_left, std::string &str_right) {
-  int strSize = (int)str.size();
-  
-  if(strSize<2) return false;
-  else{
-    int i = 1;
-    
-    while(i < strSize) {
-      str[i-1]
-          //TODO
+bool Boom::oneLetter(std::string str) {
+  if(str.size()==1) return true;
+  else return false;
+}
+
+bool Boom::repOneLetter(std::string str) {
+  if((str.size()==2) & (str[1]=='*')) return true;
+  else return false;
+}
+
+bool Boom::split_concat(const std::string str, std::string &str_left, std::string &str_right) {
+  unsigned int strSize = (unsigned int) str.size();
+
+  if (strSize < 2) return false;
+  else {
+    unsigned int i = 1;
+
+    while (i < strSize) {
+      if ((isalpha(str[i - 1]) | str[i - 1] == ')') & (isalpha(str[i]) | str[i])) {
+        str_left = str.substr(0, i);
+        str_right = str.substr(i, strSize - i);
+        return true;
+      } else i++;
     }
+    return false;
   }
+}
+
+bool Boom::split_or(const std::string str, std::string &str_left, std::string &str_right) {
+  unsigned int strSize = (unsigned int) str.size();
+
+  if (strSize < 2) return false;
+  else {
+    unsigned int i = 1;
+
+    while (i < strSize) {
+      if (str[i] == '|') {
+        str_left = str.substr(0, i);
+        str_right = str.substr(i, strSize - i);
+        return true;
+      } else i++;
+    }
+    return false;
+  }
+}
+
+void Boom::build_rep_tree(Leaf* ingang, Leaf*& goLeft) {
+  ingang->left = new Leaf;
+  ingang->left->operand = REPETITION;
+}
+
+void Boom::build_concat_tree(Leaf* ingang, Leaf*& goLeft, Leaf*& goRight) {
+  ingang->left = new Leaf;
+  ingang->left->operand = CONCAT;
+}
+
+void Boom::build_or_tree(Leaf* ingang, Leaf*& goLeft, Leaf*& goRight) {
+  ingang->left = new Leaf;
+  ingang->left->operand = OR;
+}
+
+void Boom::build_letter_tree(const std::string str, Leaf *ingang) {
+  ingang->left = new Leaf;
+  ingang->left->operand = LETTER;
+  ingang->letter = str[0];
 }
--- patroonherkenning/Boom.h
+++ patroonherkenning/Boom.h
@@ -16,50 +16,54 @@ class Boom
     };
     //Struct for the leaves/nodes
     struct Leaf {
-        Leaf();
-        Leaf* left;
-        Leaf* right;
-        
-        Operand operand;
-        char letter;
+      Leaf(){
+        left = NULL;
+        right = NULL;
+      }
+      Leaf* left;
+      Leaf* right;
+
+      Operand operand;
+      char letter;
     };
     
     //keeps track of starting point of the tree
     Leaf* root;
     
   private:
-    
+
+
     /*recursively called function that takes a (sub) string and builds a pointer tree
      * with its (sub) root attached at the pointer ingang*/
     void Tree_building(std::string str, Leaf* ingang);
     
     /*check if a substring is enclosed by breackts*/
-    bool enclosed(std::string str);
+    bool enclosed(const std::string str);
     /*check if a substring is enclosed by breackts and has repetition*/
-    bool repEnclosed(std::string str);
+    bool repEnclosed(const std::string str);
     
     /*check if the remaining substring is only a letter*/
-    bool OneLetter(std::string str);
+    bool oneLetter(const std::string str);
     /*check if a letter with repetition*/
-    bool RepOneLetter(std::string str);
+    bool repOneLetter(const std::string str);
     
     /*returns if there is a concatanation in the substring, if yes changes left and right to the
      * left and right part around the first found concatanation*/
-    bool split_concat(std::string str, std::string& str_left, std::string& str_right);
+    bool split_concat(const std::string str, std::string& str_left, std::string& str_right);
     /*returns if there is a choice (or) in the substring, if yes changes left and right to the
      * left and right part around the first found 'or'*/
-    bool split_or(std::string str, std::string& str_left, std::string& str_right);
+    bool split_or(const std::string str, std::string& str_left, std::string& str_right);
     
     /*Adds a repetition leaf, then recusively by Tree_building the 'to be repeated part' is added
      * for this to work with the recusion of Tree_building the to be attached substring needs to be
      * rememberd in Tree_building*/
-    void build_rep_tree(Leaf* ingang);
+    void build_rep_tree(Leaf* ingang, Leaf*& goLeft);
     /*Adds a leaf of type CONCAT, for this left and right substring need to be rememberd too*/
-    void build_concat_tree(Leaf* ingang);
+    void build_concat_tree(Leaf* ingang, Leaf*& goLeft, Leaf*& goRight);
     /*Adds a leaf of type OR, left and right again need to be passed along nicely*/
-    void build_or_tree(Leaf* ingang);
+    void build_or_tree(Leaf* ingang, Leaf*& goLeft, Leaf*& goRight);
     /*Build the subtree for a single letter (just a leaf)*/
-    void build_letter_tree(std::string str, Leaf* ingang)
+    void build_letter_tree(const std::string str, Leaf* ingang)
   };
 
 
